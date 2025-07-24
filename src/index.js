540be8d64a5f50365c6743e8bf47259a5ad92f27
Mikhail <liramikhails@gmail.com>
2025-07-24 18:07:17 +0300

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const nString = Math.abs(n).toString();
+  let nNew = '';
+  let Counter = nString.length;
+  while (Counter !== 0) {
+    nNew += nString[Counter - 1];
+    Counter -= 1;
+  }
+  return nNew;
 };
